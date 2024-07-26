c50fae238a8c707576ab7c44230cae1810b3dfcf
Eliezer Graber <me@eygraber.com>
2024-07-26 18:19:27 -0400

    Update deps

--- virtue-session/build.gradle.kts
+++ virtue-session/build.gradle.kts
@@ -70,3 +70,12 @@ kotlin {
     }
   }
 }
+
+configurations.all {
+  resolutionStrategy.eachDependency {
+    if(requested.group == "org.jetbrains.kotlinx" && requested.name == "atomicfu") {
+      useVersion(libs.versions.atomicfuForCmp.get())
+      because("https://youtrack.jetbrains.com/issue/CMP-5831")
+    }
+  }
+}

846f2c270e7d8d4de3f26e9d2b99a89d4254b51e
Eliezer Graber <me@eygraber.com>
2024-11-07 14:27:23 -0500

    Transition to using kotlin-inject qualifiers for Context

--- virtue-android/src/main/kotlin/com/eygraber/virtue/android/context.kt
+++
@@ -1,6 +0,0 @@
-package com.eygraber.virtue.android
-
-import android.content.Context
-
-public typealias AppContext = Context
-public typealias ActivityContext = Context
--- virtue-di-components/src/androidMain/kotlin/com/eygraber/virtue/di/components/VirtuePlatformSessionComponent.android.kt
+++ virtue-di-components/src/androidMain/kotlin/com/eygraber/virtue/di/components/VirtuePlatformSessionComponent.android.kt
@@ -1,12 +1,13 @@
 package com.eygraber.virtue.di.components
 
+import android.content.Context
 import com.eygraber.virtue.android.ActivityContext
 import me.tatarka.inject.annotations.Component
 import me.tatarka.inject.annotations.Provides
 
 @Component
 public actual abstract class VirtuePlatformSessionComponent(
-  @get:Provides public val context: ActivityContext,
+  @get:Provides @ActivityContext public val context: Context,
 ) {
   public actual companion object
 }
